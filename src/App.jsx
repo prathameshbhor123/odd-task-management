8a8120d9df077b7479e02d5f90dc109d7f3fe655
Prathamesh Bhor <prathameshbhor2001@gmail.com>
2025-05-28 14:13:08 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@
 import './App.css'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import AdminDashboaPage from './Pages/AdminPages/AdminDashboardPage'
+import AdminDashboardPage from './Pages/AdminPages/AdminDashboardPage'
 import PostTaskPage from './Pages/AdminPages/PostTaskPage'
-import ShowComplaints from './Components/Admin/ShowComplaints'
-import UpdateTask from './Components/Admin/UpdateTask'
+
 import SignupPage from './Pages/AuthPages/SignupPage'
 import HomePage from './Pages/AuthPages/HomePage'
 import LoginPages from './Pages/AuthPages/LoginPages'
@@ -14,30 +13,53 @@ import AboutUsPage from './Pages/AuthPages/AboutUsPage'
 import ContactUsPage from './Pages/AuthPages/ContactUsPage'
 import ViewEmployeeTaskDetailsPage from './Pages/EmployeePages/ViewEmployeeTaskDetailsPage'
 import EmployeeDashboardPage from './Pages/EmployeePages/EmployeeDashboardPage'
-
+import ProtectedRoute from './Components/ProtectedRout'
+import ShowComplaintsPage from './Pages/AdminPages/ShowComplaintsPage'
+import UpdateTaskPage from './Pages/AdminPages/UpdateTaskPage'
 function App() {
  
 return (
-    <>
-   
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-        <Route path='/admindashboard' element={<AdminDashboaPage/>} />
-        <Route path='/posttask' element={<PostTaskPage/>} />
-        <Route path='/showcomplaints' element={<ShowComplaints/>} />
-         <Route path='/updatetask/:id' element={<UpdateTask/>} /> 
-         <Route path='/viewtaskdetails/:id' element={<ViewTaskDetailsPage/>} />
-         <Route path='/loginpage' element={<LoginPages/>} />
-         <Route path='/aboutus' element={<AboutUsPage/>} />
-         <Route path='/contactus' element={<ContactUsPage/>} />
-        <Route path='/signup' element={<SignupPage/>} />
-         <Route path='/employeedashboard' element={<EmployeeDashboardPage/>} />
-         <Route path='/viewemployeetaskdetails/:id' element={<ViewEmployeeTaskDetailsPage/>} />
-         <Route path='/home' element={<HomePage/>} />
-         
-        </Routes>
- 
-   
+      <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/loginpage" element={<LoginPages />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/aboutus" element={<AboutUsPage />} />
+        <Route path="/contactus" element={<ContactUsPage />} />
+
+
+        {/* Admin Routes */}
+        <Route
+          path="/admindashboard"
+          element={<ProtectedRoute element={AdminDashboardPage} allowedRoles={['ADMIN']} />}
+        />
+        <Route
+          path="/posttask"
+          element={<ProtectedRoute element={PostTaskPage} allowedRoles={['ADMIN']} />}
+        />
+        <Route
+          path="/showcomplaints"
+          element={<ProtectedRoute element={ShowComplaintsPage} allowedRoles={['ADMIN']} />}
+        />
+        <Route
+          path="/updatetask/:id"
+          element={<ProtectedRoute element={UpdateTaskPage} allowedRoles={['ADMIN']} />}
+        />
+        <Route
+          path="/viewtaskdetails/:id"
+          element={<ProtectedRoute element={ViewTaskDetailsPage} allowedRoles={['ADMIN']} />}
+        />
+
+        {/* Employee Routes */}
+        <Route
+          path="/employeedashboard"
+          element={<ProtectedRoute element={EmployeeDashboardPage} allowedRoles={['EMPLOYEE']} />}
+        />
+        <Route
+          path="/viewemployeetaskdetails/:id"
+          element={<ProtectedRoute element={ViewEmployeeTaskDetailsPage} allowedRoles={['EMPLOYEE']} />}
+        />
+      </Routes>
     </>
   )
 }
